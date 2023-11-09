4dc41958384bc3281787cb7cd280cee9ab8213f4
219WD <macgunmanmc@gmail.com>
2023-11-09 16:18:36 -0300

    veamo ah 122i

--- models/server.js
+++ models/server.js
@@ -41,6 +41,7 @@ class Server {
         await dbConnection();
     }
 
+    
 
 
     middlewares() {
@@ -51,7 +52,7 @@ class Server {
         //Recibir datos .json
         this.app.use(express.json());
 
-        this.app.use(express.urlenconded({extended:true}));
+        // this.app.use(express.urlenconded({extended:true}));
 
         //Mostrar archivos publicos
         this.app.use(express.static("public")); 
