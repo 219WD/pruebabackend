656ea9c70bd179e1542a9a1532c3c5f16271f59d
219WD <macgunmanmc@gmail.com>
2023-11-09 15:50:33 -0300

    veamo ah 2i

--- models/server.js
+++ models/server.js
@@ -45,9 +45,9 @@ class Server {
         //Recibir datos .json
         this.app.use(express.json());
 
-        this.app.use(express.urlenconded({extended:true}));
+        // this.app.use(express.urlenconded({extended:true}));
 
-        
+
         //Mostrar archivos publicos
         this.app.use(express.static("public")); 
     }
