74a11a5ca34de680af64452701533f90e5d33d5e
219WD <macgunmanmc@gmail.com>
2023-11-11 13:51:38 -0300

    correccion en esquema usuario

--- models/usuario.js
+++ models/usuario.js
@@ -29,8 +29,8 @@ const UsuarioSchema = Schema({
     rol: {
         type: String,
         required: true,
-        // enum:["USER_ROLE","ADMIN_ROLE"],
-        // default: "USER_ROLE",
+        enum:["USER_ROLE","ADMIN_ROLE"],
+        default: "USER_ROLE",
     },
     estado: {
         type: Boolean,
